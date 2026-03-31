80f167e8b59d1dfe43b60b6e71d303648dbd9964
Michuuu18 <m.pielichowski@wp.pl>
2026-03-31 21:02:21 +0200

    refactor: dodanie GlobalExceptionHandler dla profesjonalnej obslugi bledow

---
+++ src/main/java/pl/Pielichowski/bank_api/exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package pl.Pielichowski.bank_api.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Map<String, Object>> handleRuntimeException(RuntimeException ex) {
+        
+        // Tworzymy strukturę naszej nowej, ładnej odpowiedzi
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.NOT_FOUND.value());
+        body.put("error", "Not Found");
+        body.put("message", ex.getMessage()); // Tutaj wpadnie tekst, np. "Nie znaleziono użytkownika o ID: 999"
+
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    }
+}
